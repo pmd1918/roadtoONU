daeb8073602d693c1688741754692b230092d90b
pmd1918 <alex.fries@phimudelta.org>
2026-01-21 14:18:22 -0500

    1-21 2:18 PM

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  output : "static",
-
-  vite:{
-    plugins: [
-      tailwindcss()
-    ]
+  output: "static",
+  integrations: [react()],
+  vite: {
+    plugins: [tailwindcss()],
+    assetsInclude: ['**/*.glb']
   }
 });
