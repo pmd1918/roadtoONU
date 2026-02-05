27f6b9eb03330b0b81fa250ff4c58ab88a7d2262
Jarvis <jarvis@thelostboysfactory.com>
2026-02-05 15:48:14 +0000

    fix: remove React from astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://roadtoonu.com',
   output: "static",
-  integrations: [react()],
   vite: {
-    plugins: [tailwindcss()],
-    assetsInclude: ['**/*.glb']
+    plugins: [tailwindcss()]
   }
 });
